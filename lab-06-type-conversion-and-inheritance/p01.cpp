d5270513e9bce2e5fffea18e3b90ff077939562a
Surav Shrestha <surav_02@live.com>
2022-07-07 13:51:41 +0545

    Add float to Distance conversion in Lab 6 p01.cpp

--- lab-06-type-conversion-and-inheritance/p01.cpp
+++ lab-06-type-conversion-and-inheritance/p01.cpp
@@ -16,21 +16,36 @@ private:
 public:
     Distance(int m, int cm) : _meter{m}, _centimeter{cm} {}
 
+    // Basic data type to user-defined data type
+    // Convert float to Distance object
+    Distance(float m)
+    {
+        _meter = m;
+        _centimeter = (m - _meter) * 100;
+    }
+
     // Overload the float operator to convert the distance to meters
     operator float()
     {
         return _meter + _centimeter / 100.0f;
     }
 
-    int getMeter() const { return _meter; }
-    int getCentimeter() const { return _centimeter; }
+    void display()
+    {
+        std::cout << _meter << "m " << _centimeter << "cm";
+    }
 };
 
 int main()
 {
     Distance d(1, 53);
+    d.display();
+    std::cout << " = " << float(d) << "m" << '\n';
 
-    std::cout << d.getMeter() << "m " << d.getCentimeter() << "cm = " << float(d) << "m" << '\n';
+    float m = 1.57;
+    Distance d2(m); // Distance(m)
+    std::cout << m << 'm' << " = ";
+    d2.display();
 
     return 0;
 }
